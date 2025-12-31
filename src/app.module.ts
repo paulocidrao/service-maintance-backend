e27a7e2bdfbe50b9b9ea349b25efbaa421fefb63
Paulo Cidrão <paulocdrao.123@gmail.com>
2025-12-31 19:16:32 -0300

    start project

---
+++ src/app.module.ts
@@ -0,0 +1,36 @@
+import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    UserModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => {
+        if (process.env.DB_TYPE === 'better-sqlite3') {
+          return {
+            type: 'better-sqlite3',
+            database: process.env.DB_DATABASE || './db.sqlite',
+            synchronize: process.env.synchronize === '1',
+            autoLoadEntities: process.env.DB_AUTO_LOAD_ENTITIES === '1',
+          };
+        }
+        return {
+          type: 'postgres',
+          host: process.env.DB_HOST,
+          port: parseInt(process.env.DB_PORT || '5432', 10),
+          username: process.env.DB_USERNAME,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_DATABASE,
+          synchronize: process.env.DB_SYNCHRONIZE === '1',
+          autoLoadEntities: process.env.DB_AUTO_LOAD_ENTITIES === '1',
+        };
+      },
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
